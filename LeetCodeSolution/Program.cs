573d028748e8281b74924ff0ad2ef1c7e2218d4a
Ivan Chepikov <ivan.chepikov@timetac.com>
2017-11-13 17:34:32 +0100

    Task_6 done.

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LeetCodeSolution.Tasks;
 
 namespace LeetCodeSolution
 {
@@ -10,6 +11,7 @@ namespace LeetCodeSolution
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
