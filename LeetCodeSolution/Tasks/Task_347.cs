3e51c228d58c0d49eb378d056452b5b632090740
Ivan Chepikov <ivan.chepikov@timetac.com>
2017-11-17 11:53:21 +0100

    Task 347 solved.

---
+++ LeetCodeSolution/Tasks/Task_347.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeetCodeSolution.Interfaces;
+
+namespace LeetCodeSolution.Tasks
+{
+    public class Task_347 : ITask<Tuple<int[], int>, IList<int>>
+    {
+        public IList<int> Run(Tuple<int[], int> input)
+        {
+            return TopKFrequent(input.Item1, input.Item2);
+        }
+
+        public IList<int> TopKFrequent(int[] nums, int k)
+        {
+            var elements = new ElemetsCounter();
+
+            foreach (var num in nums)
+                elements.AddElement(num);
+
+            return elements.GetMostFrequentItems().Take(k).ToArray();
+        }
+
+        private class ElemetsCounter
+        {
+            private class Element
+            {
+                public Element(int key)
+                {
+                    Key = key;
+                }
+
+                public int Key { get; }
+                public int Counter { get; set; }
+            }
+
+            private readonly LinkedList<Element> _orderedList = new LinkedList<Element>();
+
+            private readonly Dictionary<int, LinkedListNode<Element>> _dictionary =
+                new Dictionary<int, LinkedListNode<Element>>();
+
+            public void AddElement(int i)
+            {
+                if (_dictionary.ContainsKey(i))
+                {
+                    var node = _dictionary[i];
+                    node.Value.Counter++;
+
+                    var tempNode = node;
+
+                    while (tempNode.Next != null && tempNode.Next.Value.Counter < node.Value.Counter)
+                        tempNode = tempNode.Next;
+
+                    if (tempNode != node)
+                    {
+                        _orderedList.Remove(node);
+                        _orderedList.AddAfter(tempNode, node);
+                    }
+                }
+                else
+                {
+                    var node = new LinkedListNode<Element>(new Element(i));
+                    _orderedList.AddFirst(node);
+
+                    _dictionary[i] = node;
+                }
+            }
+
+            public IEnumerable<int> GetMostFrequentItems()
+            {
+                var node = _orderedList.Last;
+                while (node != null)
+                {
+                    yield return node.Value.Key;
+                    node = node.Previous;
+                }
+            }
+        }
+    }
+}
---
+++ SolutionTests/Tasks/Test_347.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using LeetCodeSolution.Tasks;
+using Xunit;
+
+namespace SolutionTests.Tasks
+{
+    public class Test_347
+    {
+        private readonly Task_347 _tasksolution;
+
+        public Test_347()
+        {
+            _tasksolution = new Task_347();
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCasesData))]
+        public void EdgeCasesTests(Tuple<int[], int> input, IList<int> output)
+        {
+            var result = _tasksolution.Run(input);
+            Assert.Equal(output, result);
+        }
+
+        public static IEnumerable<object[]> GetTestCasesData()
+        {
+            yield return GetDataItem(new[] {1, 1, 1, 2, 2, 3}, 2, new List<int> {1, 2});
+
+            yield return GetDataItem(new[] {3, 1, 1, 1, 2, 2, 3, 2, 2}, 2, new List<int> {2, 1});
+
+            yield return GetDataItem(new[] {3, 1, 1, 1, 2, 2, 3, 2, 2}, 0, new List<int>());
+
+            yield return GetDataItem(new[] {5, 3, 1, 1, 1, 3, 73, 1}, 1, new List<int> {1});
+        }
+
+        private static object[] GetDataItem(int[] items, int count, IList<int> output)
+        {
+            return new object[]
+            {
+                new Tuple<int[], int>(items, count),
+                output
+            };
+        }
+    }
+}
