d9902ff8469db09f310db794967534276fdadb92
Ivan Chepikov <ivan.chepikov@timetac.com>
2017-11-20 11:28:15 +0100

    160 & 23

---
+++ LeetCodeSolution/DataStructures/ListNode.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCodeSolution.DataStructures
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+}
