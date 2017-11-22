fae380cbdd8ee94e94b916ee389fca5551f5e840
Ivan Chepikov <ivan.chepikov@timetac.com>
2017-11-22 12:39:01 +0100

    Task 4 solved

---
+++ SolutionTests/Tasks/Test_4.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using LeetCodeSolution.Tasks;
+using Xunit;
+
+namespace SolutionTests.Tasks
+{
+    public class Test_4 : TaskTestBase<Tuple<int[], int[]>, double>
+    {
+        public Test_4() : base(new Task_4())
+        {
+        }
+
+        [Theory]
+        [MemberData(nameof(GetCommonCasesData))]
+        public void CommonTests(Tuple<int[], int[]> input, double output)
+        {
+            RunTest(input, output);
+        }
+
+        [Fact]
+        public void FailOnEmptyArrays()
+        {
+            var exception = Record.Exception(() =>
+            {
+                RunTest(new Tuple<int[], int[]>(new int[0], new int[0]), 0);
+            });
+            
+            Assert.NotNull(exception);
+        }
+
+        public static IEnumerable<object[]> GetCommonCasesData()
+        {
+            yield return PrepareInputOutput(new[] {1}, new int[0], 1);
+            yield return PrepareInputOutput(new int[0], new[] {1}, 1);
+            yield return PrepareInputOutput(new[] { 1 }, new[] { 1 }, 1);
+            
+            yield return PrepareInputOutput(new[] {1, 2, 5}, new[] {1, 2, 5}, 2);
+
+            yield return PrepareInputOutput(new[] {1, 2, 5}, new[] {10, 12, 15, 16}, 10);
+
+            yield return PrepareInputOutput(new[] {10}, new[] {15}, 12.5);
+
+            yield return PrepareInputOutput(new[] {0, 1, 10}, new[] {15, 22, 77}, 12.5);
+
+            yield return PrepareInputOutput(new[] {11}, new[] {1, 15}, 11);
+
+            yield return PrepareInputOutput(new[] {1, 3}, new[] {2}, 2);
+
+            yield return PrepareInputOutput(new[] {1, 3}, new[] {2, 4}, 2.5);
+
+            yield return PrepareInputOutput(new[] {1}, new[] {2, 4, 5, 6, 8, 9, 22, 999}, 6);
+
+            yield return PrepareInputOutput(new[] {1, 3, 6, 9, 22, 55, 88, 197},
+                new[] {2, 4, 5, 8, 9, 111, 468, 989, 2586}, 9);
+        }
+    }
+}
