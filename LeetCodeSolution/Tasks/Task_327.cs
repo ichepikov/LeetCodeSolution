8928db7df11edc7a39d0321628f0e2cce4bba467
Ivan Chepikov <ivan.chepikov@timetac.com>
2017-11-28 10:56:29 +0100

    Task 49 solved.

---
+++ LeetCodeSolution/Tasks/Task_327.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LeetCodeSolution.Interfaces;
+
+namespace LeetCodeSolution.Tasks
+{
+    class Task_327 : ITask<Tuple<int[], int, int>, int>
+    {
+        public int Run(Tuple<int[], int, int> input)
+        {
+            return CountRangeSum(input.Item1, input.Item2, input.Item3);
+        }
+
+        public int CountRangeSum(int[] nums, int lower, int upper)
+        {
+            int i = 0, j = 0;
+
+
+            var result = 0;
+            while (i < nums.Length && j < nums.Length)
+            {
+                
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolution/Tasks/Task_49.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeetCodeSolution.Interfaces;
+
+namespace LeetCodeSolution.Tasks
+{
+    public class Task_49 : ITask<String[], IList<IList<string>>>
+    {
+        public IList<IList<string>> Run(string[] input)
+        {
+            return GroupAnagrams(input);
+        }
+
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            return strs.Select(e => new {Key = new string(e.OrderBy(c => c).ToArray()), value = e})
+                .GroupBy(e => e.Key, e => e.value).Select(e => (IList<string>) e.ToList()).ToList();
+        }
+    }
+}
